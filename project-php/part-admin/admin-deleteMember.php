df455faa2532e013959cba4b0c57984a64ad792c
punkaze <punkaze.imp@gmail.com>
2017-12-11 03:10:55 +0700

    เปลี่น background

--- project-php/part-admin/admin-deleteMember.php
+++ project-php/part-admin/admin-deleteMember.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin Page</title>
-    <link rel="stylesheet" href="../others/index.css">
+    <link rel="stylesheet" href="../others/style.css">
 </head>
 <body>
     <?php
